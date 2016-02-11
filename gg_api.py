ed2d0b119f4a95001ed0a5e189f7b219b4353869
brownrout <ericbrownrout2016@u.northwestern.edu>
2016-02-11 13:17:30 -0600

    add CLI

--- gg_api.py
+++ gg_api.py
@@ -1,4 +1,8 @@
+import sys
+import nltk
+from preprocessing import *
 
+tweets = []
 
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
@@ -6,42 +10,49 @@ def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
-    return hosts
+    print "Unimplemented"
+    return #hosts
 
 def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
-    return awards
+    print "Unimplemented"
+    return #awards
 
 def get_nominees(year):
     '''Nominees is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change
     the name of this function or what it returns.'''
     # Your code here
-    return nominees
+    print "Unimplemented"
+    return #nominees
 
 def get_winners(year):
     '''Winners is a dictionary with the hard coded award
     names as keys, and each entry containing a single string.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
-    return winners
+    print "Unimplemented"
+    return #winners
 
 def get_presenters(year):
     '''Presenters is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change the
     name of this function or what it returns.'''
     # Your code here
-    return presenters
+    print "Unimplemented"
+    return #presenters
 
 def pre_ceremony():
     '''This function loads/fetches/processes any data your program
     will use, and stores that data in your DB or in a json, csv, or
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
-    # Your code here
-    print "Pre-ceremony processing complete."
+    user_input = raw_input("Which corpus to process: ")
+    global tweets
+    tweets = getTweets(user_input)
+    print "Pre-ceremony processing complete.\n"
     return
 
 def main():
@@ -50,7 +61,17 @@ def main():
     and then run gg_api.main(). This is the second thing the TA will
     run when grading. Do NOT change the name of this function or
     what it returns.'''
-    # Your code here
+    pre_ceremony()
+    while True:
+        print "\nOptions:\n1. Get Hosts\n2. Get Awards\n3. Get Nominees\n4. Get Winners\n5. Get Presenters\n"
+        user_input = input("Choose a function: ")
+        if (user_input == 1): {get_hosts(2013)}
+        elif (user_input == 2): {get_awards(2013)}
+        elif (user_input == 3): {get_nominees(2013)}
+        elif (user_input == 4): {get_winners(2013)}
+        elif (user_input == 5): {get_presenters(2013)}
+        else: print "invalid choice\n"
+    
     return
 
 if __name__ == '__main__':
--- preprocessing.py
+++ preprocessing.py
@@ -2,17 +2,20 @@ import json
 import sys
 import nltk
 
-def getTweets(file):
-    decoded_response = file.read()
-    jsonData=json.loads(decoded_response)
-    text = []
-    for item in jsonData:
-        name = item.get("text")
-        text.append(name)
+def getTweets(filename):
+	corpus = open(filename)
+	decoded_response = corpus.read()
+	jsonData=json.loads(decoded_response)
+	text = []
+	for item in jsonData:
+		name = item.get("text")
+		text.append(name)
 
-    tokenized_tweets = []
+	tokenized_tweets = []
 
-    for tweet in text:
-        tokenized_tweets.append(nltk.wordpunct_tokenize(tweet))
+	for tweet in text:
+		tokenized_tweets.append(nltk.wordpunct_tokenize(tweet))
+		
+	return tokenized_tweets
 
 #    print tokenized_tweets[0][0]
