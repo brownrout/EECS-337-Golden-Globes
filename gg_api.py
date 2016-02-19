4c2bf7ef2067b96ce769aedcc32318f54b854245
brownrout <ericbrownrout2016@u.northwestern.edu>
2016-02-18 18:14:33 -0600

    update interface for either year

--- gg_api.py
+++ gg_api.py
@@ -5,8 +5,10 @@ from nltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk
 from nameparser.parser import HumanName
 from collections import Counter
 
-tweets = []
-officialTweets = []
+tweets13 = []
+officialTweets13 = []
+tweets15 = []
+officialTweets15 = []
 
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
@@ -67,34 +69,52 @@ def get_hosts(year):
 def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
-    
-    awards = []
 
-    award_tweets = []
-    #mini-series may give problems
     award_words = ['Best', 'best', 'Motion', 'motion', 'Picture', 'picture', 'Drama', 'drama', 'Performance', 'performance', 'Actress', 'actress', 'Actor', 'actor','Comedy', 'comedy', 'Musical', 'musical', 'Animated', 'animated', 'Feature', 'feature', 'Film', 'film', 'Foreign', 'foreign', 'Language', 'language', 'Supporting', 'supporting', 'Role', 'role', 'Director', 'director', 'Screenplay', 'screenplay', 'Original', 'orginal', 'Score', 'score', 'Song', 'song', 'Television', 'television', 'Series', 'series', 'Mini-series',  'mini-series']
     helper_words = ['by','By','An','an','In','in','A','a','For','for','-',':','Or','or']
-
-    for tweet in officialTweets:
-        if len(set(award_words).intersection(set(tweet))) > 2:
-            award_tweets.append(sorted(set(tweet), key=lambda x: tweet.index(x)))
-
-    for tweet in award_tweets:
-        temp = []
-        for word in tweet:
-            # if word in award_words or word in helper_words:
-            if word in award_words:
-                temp.append(word.lower())
-        awardString = ' '.join(sorted(set(temp), key=lambda x: temp.index(x)))
-        if awardString not in awards:
-            awards.append(awardString)
-
-    for x in awards:
-        if x.split()[0] != 'best':
-            awards.remove(x)
-        print x
-
-    return awards
+    
+    if year == '2013':
+        print "Getting awards for 2013..." + '\n'
+        awards = []
+        award_tweets = []
+        for tweet in officialTweets13:
+            if len(set(award_words).intersection(set(tweet))) > 2:
+                award_tweets.append(sorted(set(tweet), key=lambda x: tweet.index(x)))
+        for tweet in award_tweets:
+            temp = []
+            for word in tweet:
+                # if word in award_words or word in helper_words:
+                if word in award_words:
+                    temp.append(word.lower())
+            awardString = ' '.join(sorted(set(temp), key=lambda x: temp.index(x)))
+            if awardString not in awards:
+                awards.append(awardString)
+        for x in awards:
+            if x.split()[0] != 'best':
+                awards.remove(x)
+            print x
+        return awards
+    if year == '2015':
+        print "Getting awards for 2015..." + '\n'
+        awards = []
+        award_tweets = []
+        for tweet in officialTweets13:
+            if len(set(award_words).intersection(set(tweet))) > 2:
+                award_tweets.append(sorted(set(tweet), key=lambda x: tweet.index(x)))
+        for tweet in award_tweets:
+            temp = []
+            for word in tweet:
+                # if word in award_words or word in helper_words:
+                if word in award_words:
+                    temp.append(word.lower())
+            awardString = ' '.join(sorted(set(temp), key=lambda x: temp.index(x)))
+            if awardString not in awards:
+                awards.append(awardString)
+        for x in awards:
+            if x.split()[0] != 'best':
+                awards.remove(x)
+            print x
+        return awards
 
 def get_nominees(year):
     '''Nominees is a dictionary with the hard coded award
@@ -177,10 +197,11 @@ def pre_ceremony():
     will use, and stores that data in your DB or in a json, csv, or
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
-    user_input = raw_input("Which corpus to process: ")
-    global tweets
-    global officialTweets
-    tweets, officialTweets = getTweets(user_input)
+    global tweets13
+    global officialTweets13
+    global tweets15
+    global officialTweets15
+    tweets13, officialTweets13, tweets15, officialTweets15 = getTweets()
     print "Pre-ceremony processing complete.\n"
     return
 
@@ -192,12 +213,13 @@ def main():
     what it returns.'''
     pre_ceremony()
     while True:
+        year = raw_input("Which year: ")
         print "\nOptions:\n1. Get Hosts\n2. Get Awards\n3. Get Nominees\n4. Get Winners\n5. Get Presenters\n"
         user_input = input("Choose a function: ")
         if (user_input == 1):
-            hosts = get_hosts(2013)
+            hosts = get_hosts(year)
         elif (user_input == 2):
-            awards = get_awards(2013)
+            awards = get_awards(year)
         elif (user_input == 3): {get_nominees(2013)}
         elif (user_input == 4): {get_winners(2013)}
         elif (user_input == 5): {get_presenters(2013)}
--- preprocessing.py
+++ preprocessing.py
@@ -8,18 +8,36 @@ from nltk.tokenize import RegexpTokenizer
 stopwordsList = stopwords.words('english')
 
 tokenized_tweets = []
-official_tweets = []
+tokenized_tweets2 = []
 
-def getTweets(filename):
-	corpus = open(filename)
+official_tweets = []
+official_tweets2 = []
+
+def getTweets():
+	
+	corpus = open('gg2013.json')
+	corpus2 = open('gg2015.json')
+	
 	decoded_response = corpus.read()
+	decoded_response2 = corpus2.read()
+
 	jsonData=json.loads(decoded_response)
+	jsonData2=json.loads(decoded_response2)
+	
 	text = []
+	text2 = []
+
 	for item in jsonData:
 		name = item.get("text")
 		text.append(name)
 
+	for item in jsonData2:
+		name = item.get("text")
+		text2.append(name)
+
 	tokenized_tweets = []
+	tokenized_tweets2 = []
+
 	tokenizer = RegexpTokenizer(r'\w+')
 
 	for tweet in text:
@@ -30,9 +48,24 @@ def getTweets(filename):
 		tweet = tokenizer.tokenize(tweet)
 		tokenized_tweets.append(tweet)
 
+	for tweet in text2:
+		if '@goldenglobes' in tweet and 'RT' not in tweet:
+			temp_tweet = tokenizer.tokenize(tweet)
+			if u'goldenglobes' == temp_tweet[0]:
+			 	official_tweets2.append(temp_tweet)
+		tweet = tokenizer.tokenize(tweet)
+		tokenized_tweets2.append(tweet)
+
+
+
 	for tweet in tokenized_tweets:
 		for token in tweet:
 			if token.lower() in stopwordsList:
 				tweet.remove(token)
 
-	return tokenized_tweets, official_tweets
+	for tweet in tokenized_tweets2:
+		for token in tweet:
+			if token.lower() in stopwordsList:
+				tweet.remove(token)
+
+	return tokenized_tweets, official_tweets, tokenized_tweets2, official_tweets2
